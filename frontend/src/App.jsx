fb0d515f8467f9b9af0d64b57d3a6b62bd69a4b1
JameNori <jame_nori@hotmail.com>
2026-06-16 17:42:31 +0700

    feat: add WhyChooseUs component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <WhyChooseUs />
     </div>
   );
 }
