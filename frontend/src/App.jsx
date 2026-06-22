7bd99b28220e1d722d44921407824b2e1d34b020
JameNori <jame_nori@hotmail.com>
2026-06-22 16:16:16 +0700

    feat: add GalleryPreview component with mock images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import WhyChooseUs from "./components/WhyChooseUs";
 import Services from "./components/Services";
+import GalleryPreview from "./components/GalleryPreview";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <WhyChooseUs />
       <Services />
+      <GalleryPreview />
     </div>
   );
 }
