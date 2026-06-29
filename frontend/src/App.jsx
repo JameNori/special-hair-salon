be93bcd079630e9fc741b5843c7d836a27120393
JameNori <jame_nori@hotmail.com>
2026-06-29 13:40:15 +0700

    refactor(frontend): move landing page content to pages/LandingPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,5 @@
-import Navbar from "./components/layout/Navbar";
-import Hero from "./components/sections/Hero";
-import WhyChooseUs from "./components/sections/WhyChooseUs";
-import Services from "./components/sections/Services";
-import GalleryPreview from "./components/sections/GalleryPreview";
-import StylistsPreview from "./components/sections/StylistsPreview";
-import Footer from "./components/layout/Footer";
+import LandingPage from "./pages/LandingPage";
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <WhyChooseUs />
-      <Services />
-      <GalleryPreview />
-      <StylistsPreview />
-      <Footer />
-    </div>
-  );
+export default function App() {
+  return <LandingPage />;
 }
-
-export default App;
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,21 @@
+import Navbar from "../components/layout/Navbar";
+import Hero from "../components/sections/Hero";
+import WhyChooseUs from "../components/sections/WhyChooseUs";
+import Services from "../components/sections/Services";
+import GalleryPreview from "../components/sections/GalleryPreview";
+import StylistsPreview from "../components/sections/StylistsPreview";
+import Footer from "../components/layout/Footer";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <WhyChooseUs />
+      <Services />
+      <GalleryPreview />
+      <StylistsPreview />
+      <Footer />
+    </div>
+  );
+}
